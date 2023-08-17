03b598360493ad49ee028f5633e1b63a21b366b6
lakshit rajpal <lakshitrajpal@gmail.com>
2023-08-17 13:15:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.post('/client-requests', routes.client-requests);
 app.post('/activity', activity );
 
 // Custom Hello World Activity Routes
-app.post('/journeybuilder/save/', activity.save );
-app.post('/journeybuilder/validate/', activity.validate );
-app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+app.post('/api/journeybuilder/save/', activity.save );
+app.post('/api/journeybuilder/validate/', activity.validate );
+app.post('/api/journeybuilder/publish/', activity.publish );
+app.post('/api/journeybuilder/execute/', activity.execute );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
