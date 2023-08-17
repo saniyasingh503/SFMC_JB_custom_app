b2737c5839994fd4f66e00df4cdbdc7d0539a10f
lakshit rajpal <lakshitrajpal@gmail.com>
2023-08-17 14:08:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post('/activity', activity );
 // Custom Hello World Activity Routes
 //app.post('/api/journeybuilder/save/', activity.save );
 //app.post('/api/journeybuilder/validate/', activity.validate );
-app.post('/publish/', activity.publish );
+app.post('/publish', activity.publish );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
