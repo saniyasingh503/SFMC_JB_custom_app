eed1ebd39499c0da0d78c118a851ab51f8592990
lakshit rajpal <lakshitrajpal@gmail.com>
2023-08-17 14:43:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ app.get('/', routes.index );
 
 // Custom Routes for MC
 //app.post('/journeybuilder/save/', activity.save );
-//app.post('/journeybuilder/validate/', activity.validate );
-app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+//app.post('/journeybuilder/validate/', activity.validate);
+app.post('/journeybuilder/publish/', activity.publish);
+app.post('/journeybuilder/execute/', activity.execute);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
